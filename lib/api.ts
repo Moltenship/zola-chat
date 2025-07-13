770233f781a909f0e5975c0ef7bae68caaf52977
Timofey Maximov <moltenship7750@gmail.com>
2025-07-14 01:47:03 +0400

    Fix auth url (i hope)

--- lib/api.ts
+++ lib/api.ts
@@ -106,17 +106,8 @@ export async function signInWithGoogle(supabase: SupabaseClient) {
   try {
     const isDev = process.env.NODE_ENV === "development"
 
-    let baseUrl;
+    const baseUrl = isDev ? "http://localhost:3000" : APP_DOMAIN;
 
-    if (isDev) {
-      baseUrl = "http://localhost:3000"
-    }
-
-    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-      baseUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    } else {
-      baseUrl = APP_DOMAIN
-    }
 
     // // Get base URL dynamically (will work in both browser and server environments)
     // const baseUrl = isDev
